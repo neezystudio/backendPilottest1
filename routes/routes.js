e46e79a7594a1485a1e26c434780d9d21593a45a
neezystudio <victorneez6@gmail.com>
2023-12-05 10:39:43 +0300

    CRUD model structuring

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,27 @@
 // routes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllItems, getItemById, createItem, updateItem, deleteItem } = require('./controller');
+const {
+  getAllProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
+} = require("./controller");
 
 // Get all items
-router.get('/items', getAllItems);
+router.get("/projects", getAllProjects);
 
 // Get a single item by ID
-router.get('/items/:id', getItemById);
+router.get("/projects/:id", getProjectById);
 
 // Create a new item
-router.post('/items', createItem);
+router.post("/projects", createProject);
 
 // Update an item by ID
-router.put('/items/:id', updateItem);
+router.put("/projects/:id", updateProject);
 
 // Delete an item by ID
-router.delete('/items/:id', deleteItem);
+router.delete("/projects/:id", deleteProject);
 
 module.exports = router;
