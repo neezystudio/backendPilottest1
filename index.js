f37749ac4a6b4b114bf475ed7a1d848753314a90
neezystudio <victorneez6@gmail.com>
2023-12-07 10:11:57 +0300

    wip

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const routes = require('./routes/routes'); //
 // const app = require('./app');
 
 const cors = require("cors");
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
+// Use bodyParser middleware to parse JSON and form data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routes);  // You can specify a different base path if needed
 
 const dbURI =
   "mongodb+srv://victorneez6:123Mruc%21%40%23@cluster0.akev7kv.mongodb.net/?retryWrites=true&w=majority";
--- models/projects.js
+++ models/projects.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const userSchema = new mongoose.Schema({
-    projectName: { type: String, required: true },
-    projectStatus: { type: Number, required: true},
-    projectCost: { type: Number, required: true}
-})
+const projectSchema = new mongoose.Schema({
+  projectName: { type: String, required: true },
+  projectStatus: { type: Number, required: true },
+  projectCost: { type: Number, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
